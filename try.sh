20582bc2a188a33c842d17c45566e2bffdccb7ca
Jeka Kovtun <jeka.ns@gmail.com>
2019-11-07 02:05:25 +0100

    Connecting to AMQP

--- try.sh
+++ try.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+redis-cli -p 7007 MODULE UNLOAD key_evt_amqp
 redis-cli -p 7007 MODULE LOAD /home/build_release/libredis_keyspace_amqp.so
 redis-cli -p 7007 keamqp.rand
 redis-cli -p 7007 keamqp.rand
