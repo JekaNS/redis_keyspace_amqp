9629843213a5c102eb29afb9b7880f3cc9aad535
Jeka Kovtun <jeka.ns@gmail.com>
2019-11-20 10:09:32 +0100

    CMAKE_C_STANDARD 99

--- try.sh
+++ try.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-redis-cli MODULE LOAD ./cmake-build-debug/libredis_keyspace_amqp.dylib
-redis-cli keamqp.connect
+redis-cli MODULE LOAD ./cmake-build-debug/libredis_keyspace_amqp.dylib 127.0.0.1 5672 guest guest 20 redis redis_keyspace_events 2 ALL
 redis-cli SETEX test_key_name 1 "test_value"
 
